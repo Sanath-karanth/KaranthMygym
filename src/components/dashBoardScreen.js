5566833ec5a5515eecf6fe9f3d3455f4fb2be55c
Sanath S Karanth <sanathsk97@gmail.com>
2023-09-28 18:59:19 +0530

    my gym about remove update

--- src/components/dashBoardScreen.js
+++ src/components/dashBoardScreen.js
@@ -1,139 +1,198 @@
-import React, { memo, Fragment } from 'react';
-import '../css/dashboard.css'
-import { useNavigate  } from "react-router-dom";
-import {Container,Row,Col} from 'react-bootstrap'
+import React, { memo, Fragment } from "react";
+import "../css/dashboard.css";
+import { useNavigate } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 const DashboardScreen = memo(() => {
+  const navigate = useNavigate();
 
-    const navigate  = useNavigate();
+  const cardClick = (clickval) => {
+    navigate("/exercise", {
+      state: {
+        navigationcontent: clickval,
+      },
+    });
+  };
 
-    const cardClick = (clickval) => {
-        navigate("/exercise", 
-                    { state: 
-                        {
-                            navigationcontent: clickval 
-                        }
-                    }
-                );
-    }
-
-    const aboutClick = () => {
-        navigate("/about");
-    }
+  const aboutClick = () => {
+    navigate("/about");
+  };
 
   return (
     <Fragment>
-        <div className='MainContainer-dashboard'>
-            <div className='SubContainer-dashboard'>
-                <div className='HeadContainer-dashboard' 
-                style={
-                    { 
-                        backgroundImage: "linear-gradient(to bottom, rgba(40, 48, 72, 1), rgba(28, 216, 210, 0.7)),url('../images/background0.jpg')", 
-                    }
-                }>
-                    <Container fluid className='container-guttersfordashboard'>
-                        <div className='db-header-cont'>
-                            <Row className='gx-0'>
-                                <Col xs={10} sm={10} md={8} lg={8} xl={8} xxl={8}>
-                                    <div className='db-headerTitle'>
-                                        <img src='../images/buildicon1.png' alt='Logo'></img>
-                                        <h1>MY GYM</h1>
-                                    </div>
-                                </Col>
-                                <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
-                                    <div className='db-headerAbout' onClick={aboutClick}>
+      <div className="MainContainer-dashboard">
+        <div className="SubContainer-dashboard">
+          <div
+            className="HeadContainer-dashboard"
+            style={{
+              backgroundImage:
+                "linear-gradient(to bottom, rgba(40, 48, 72, 1), rgba(28, 216, 210, 0.7)),url('../images/background0.jpg')",
+            }}
+          >
+            <Container fluid className="container-guttersfordashboard">
+              <div className="db-header-cont">
+                <Row className="gx-0">
+                  <Col xs={10} sm={10} md={8} lg={8} xl={8} xxl={8}>
+                    <div className="db-headerTitle">
+                      <img src="../images/buildicon1.png" alt="Logo"></img>
+                      <h1>MY GYM</h1>
+                    </div>
+                  </Col>
+                  <Col xs={2} sm={2} md={4} lg={4} xl={4} xxl={4}>
+                    {/* <div className='db-headerAbout' onClick={aboutClick}>
                                         <h3>About</h3>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
+                                    </div> */}
+                  </Col>
+                </Row>
+              </div>
 
-                        <div className='db-cardmain-cont'>
-                            <Row className='gx-0'>
-                                <Col xs={0} sm={0} md={4} lg={4} xl={4} xxl={4} className='db-cardCol'>
-                                    <div className='db-cardLeft'>
-                                        
-                                    </div>
-                                </Col>
-                                <Col xs={12} sm={12} md={8} lg={8} xl={8} xxl={8} className='db-cardCol'>
-                                    <div className='db-cardRight'>
-                                        <Row className='gx-0'>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Thighs')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Thighs</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Chest')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Chest</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Lats')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Lats</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Biseps')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Biceps</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Shoulder')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Shoulder</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Triceps')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>Triceps</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('ABS')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>ABS</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                            <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
-                                                <div className='db-card-cont'>
-                                                    <div className='db-card' onClick={() => cardClick('Forearm')}>
-                                                        <img src='../images/dumbbelllogo.gif' alt='Logo'></img>
-                                                        <h2>ForeArms</h2>
-                                                    </div>
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Container>
-
-                </div>
-            </div>
+              <div className="db-cardmain-cont">
+                <Row className="gx-0">
+                  <Col
+                    xs={0}
+                    sm={0}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    xxl={4}
+                    className="db-cardCol"
+                  >
+                    <div className="db-cardLeft"></div>
+                  </Col>
+                  <Col
+                    xs={12}
+                    sm={12}
+                    md={8}
+                    lg={8}
+                    xl={8}
+                    xxl={8}
+                    className="db-cardCol"
+                  >
+                    <div className="db-cardRight">
+                      <Row className="gx-0">
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Thighs")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Thighs</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Chest")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Chest</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Lats")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Lats</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Biseps")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Biceps</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Shoulder")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Shoulder</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Triceps")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>Triceps</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("ABS")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>ABS</h2>
+                            </div>
+                          </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} lg={5} xl={4} xxl={4}>
+                          <div className="db-card-cont">
+                            <div
+                              className="db-card"
+                              onClick={() => cardClick("Forearm")}
+                            >
+                              <img
+                                src="../images/dumbbelllogo.gif"
+                                alt="Logo"
+                              ></img>
+                              <h2>ForeArms</h2>
+                            </div>
+                          </div>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Container>
+          </div>
         </div>
+      </div>
     </Fragment>
-  )
-})
+  );
+});
 
-export default DashboardScreen
+export default DashboardScreen;
